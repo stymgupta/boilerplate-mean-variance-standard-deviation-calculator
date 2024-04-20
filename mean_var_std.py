45c8b834a71a9ff67315339ef89222276090ef4b
VAIBHAV KUMAR <satyamraj44@outlook.com>
2024-04-20 06:42:20 +0000

    success

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,6 +1,26 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    arr = np.array(list).reshape((3,3)) # Convert list to a NumPy npay and reshape
+    mean = [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(arr).tolist()]
+    variance = [np.var(arr, axis=0).tolist(), np.var(arr, axis=1).tolist(), np.var(arr).tolist()]
+    std_dev = [np.std(arr, axis=0).tolist(), np.std(arr, axis=1).tolist(), np.std(arr).tolist()]
+    max_val = [np.max(arr, axis=0).tolist(), np.max(arr, axis=1).tolist(), np.max(arr).tolist()]
+    min_val = [np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(arr).tolist()]
+    sum_val = [np.sum(arr, axis=0).tolist(), np.sum(arr, axis=1).tolist(), np.sum(arr).tolist()]
+    
+    calculations = {
+        
+        'mean': mean,
+        'variance': variance,
+        'standard deviation': std_dev,
+        'max': max_val,
+        'min': min_val,
+        'sum': sum_val
+    }
 
 
 
